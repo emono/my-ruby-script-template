425ad29925d5b7099d34b3d7d5ff6e29fb16ff6d
emono <ababababa0222@gmail.com>
2019-05-16 11:15:26 +0900

    fix notify

--- lib/hogehoge.rb
+++ lib/hogehoge.rb
@@ -2,3 +2,5 @@ require_relative 'hogehoge/notify'
 require 'line-notify-client'
 require 'slack/incoming/webhooks'
 # execute scripts
+notify = Notify.new
+notify.slack_post("aa")
--- lib/hogehoge/notify.rb
+++ lib/hogehoge/notify.rb
@@ -1,6 +1,6 @@
-class Notify  
-  attr_reader :slack_webhook, 
-  def initialize(post_hash)
+class Notify
+  attr_reader :slack_webhook
+  def initialize
     @slack_webhook = Slack::Incoming::Webhooks.new(ENV['SLACK_WEBHOOK_URL'])
   end
 
@@ -8,13 +8,10 @@ class Notify
     Line::Notify::Client.message(message: message_text)
   end
 
-  def slack_post
+  def slack_post(message_text)
     message = "```#{message_text}```"
     slack_webhook.post(message)
   end
 
   private
-    def message_text
-      "hogehoge"
-    end
 end
